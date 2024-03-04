138314f9a015a750f1b8543ef51ebce53c7a465f
ashrafali2000 <ali4282271@gmail.com>
2024-03-04 14:14:48 +0500

    add my image

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/myimg.jpeg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Hi! Ashraf ali" />
     </div>
   </header>
 
@@ -36,6 +42,7 @@ header {
 
   .logo {
     margin: 0 2rem 0 0;
+    border-radius: 50%;
   }
 
   header .wrapper {
