475d420c81690b1988a17815415859fd026c99b3
ashrafali2000 <122337346+ashrafali2000@users.noreply.github.com>
2024-03-09 13:02:11 +0500

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,13 @@ import TheWelcome from "./components/TheWelcome.vue";
 import NavBar from "./components/NavBar.vue";
 </script>
 
-<template>
-<NavBar />
+<template >
+<div class="flex flex-col bg-green-500 w-[100%]">
 
+<NavBar />
   <header>
     <img
+
       alt="Vue logo"
       class="logo"
       src="./assets/myimg.jpeg"
@@ -24,9 +26,12 @@ import NavBar from "./components/NavBar.vue";
   <main>
     <TheWelcome />
   </main>
+</div>
+
 </template>
 
-<style scoped>
+<style >
+
 header {
   line-height: 1.5;
 }
@@ -36,22 +41,5 @@ header {
   margin: 0 auto 2rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-    border-radius: 50%;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
