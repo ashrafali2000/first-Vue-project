0bc1fbaf66a7d3cdd9928ffb1e6c94d3e9c90856
ashrafali2000 <122337346+ashrafali2000@users.noreply.github.com>
2024-03-09 12:14:47 +0500

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
 import HelloWorld from "./components/HelloWorld.vue";
 import TheWelcome from "./components/TheWelcome.vue";
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
+<NavBar />
+
   <header>
     <img
       alt="Vue logo"
